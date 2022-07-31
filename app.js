7c54ccef95eb06959d3bfdb0a32a8bf2fe79f71c
Alger Makiputin <algerzxc@gmail.com>
2022-07-31 23:18:15 +0800

    add new function for fetching stock price

--- app.js
+++ app.js
@@ -6,39 +6,62 @@ const jsdom = require('jsdom');
 require('dotenv').config();
 
 const url = process.env.liveURL; 
+const pricesURL = process.env.pricesURL; 
 
-async function getCompanies() {
-
-    var test = [];
-    await axios.post(url, {
-        companyId:'',
-        keyword: '',
-        sector: 'ALL',
-        subsector: 'ALL',
-        json:true
-    }).then(res => { 
-        const dom = new jsdom.JSDOM(res.data);
+async function getCompanies(pageNo) { 
+    return await axios.post(`${url}?pageNo=${pageNo}`, {}, { 
+            headers: { 'Content-Type': 'application/json', } 
+        }
+    ).then(res => {    
+        const dom = new jsdom.JSDOM(res.data); 
         const table = dom.window.document.querySelector(".list")
         const tbody = table.querySelector("tbody");
         const rows = tbody.querySelectorAll("tr");
-        rows.forEach((element) => {
+        const data = [];
+        rows.forEach((element) => { 
+            const anchorTexts = element.querySelector("td a").getAttribute('onClick').split("'");
+            const companyId = anchorTexts[1];
+            const securityId = anchorTexts[3];
             const name = element.querySelectorAll("td")[0].textContent;
             const symbol = element.querySelectorAll("td")[1].textContent;
             const sector = element.querySelectorAll("td")[2].textContent;
             const subsector = element.querySelectorAll("td")[3].textContent;
-            const listingDate = element.querySelectorAll("td")[4].textContent; 
-        
+            const listingDate = element.querySelectorAll("td")[4].textContent;
+            data.push({
+                name,
+                symbol,
+                sector,
+                subsector,
+                listingDate,
+                companyId,
+                securityId,
+            })    
         })
+
+        return data;
 
     }).catch(err => {
         console.log(err)
     })
 }
 
-app.get('/', async (req, res) => {
+async function getPrice(companyId, securityId) { 
     
-    getCompanies();
-    res.send();
+    return await axios.post(`${pricesURL}?`, {
+        cmpy_id: companyId,
+        security_id: securityId,
+        startDate: "07-25-2022",
+        endDate: "07-29-2022"
+    }).then(result => {
+        console.log(result.data);
+    }).catch(error => {
+        console.log(error);
+    })
+}
+
+app.get('/', async (req, res) => { 
+    await getPrice(29,146)
+    res.send(); 
 })
 
 
