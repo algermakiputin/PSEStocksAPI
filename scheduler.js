60811ca857b689581782e2921049e9d38156b6aa
Alger Makiputin <algerzxc@gmail.com>
2022-08-20 22:03:09 +0800

    setup scheduler time

--- scheduler.js
+++ scheduler.js
@@ -1,11 +1,9 @@
 const { updatePrices } = require('./functions');
 const { CronJob } = require('cron');
-//00 00 16 * * 1-5
+
 const priceUpdates = new CronJob(
-    '*/3 * * * * 1-6',
-    function() {
-        console.log('cron is running')
-    },
+    '00 00 16 * * 1-5',
+    updatePrices,
     null,
     false,
     'Asia/Manila'
